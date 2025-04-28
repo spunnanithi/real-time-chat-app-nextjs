5f13eb8533116d1e0dd3329e69f93baf227939db
Sirasit Punnanithi <spunnanithi@gmail.com>
2025-04-28 15:39:11 -0700

    Created Dialog and Form components to add friends

---
+++ app/(root)/friends/_components/AddFriendDialog.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import React from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Button } from "@/components/ui/button";
+import { UserPlus } from "lucide-react";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+type Props = {};
+
+const addFriendFormSchema = z.object({
+	email: z
+		.string()
+		.min(1, { message: "This field can't be empty" })
+		.email("Please enter a valid email"),
+});
+
+const AddFriendDialog = (props: Props) => {
+	const form = useForm<z.infer<typeof addFriendFormSchema>>({
+		resolver: zodResolver(addFriendFormSchema),
+		defaultValues: {
+			email: "",
+		},
+	});
+
+	const handleSubmit = () => {};
+
+	return (
+		<Dialog>
+			<Tooltip>
+				<TooltipTrigger>
+					<Button size="icon" variant="outline">
+						<DialogTrigger>
+							<UserPlus />
+						</DialogTrigger>
+					</Button>
+				</TooltipTrigger>
+				<TooltipContent>
+					<p>Add Friend</p>
+				</TooltipContent>
+			</Tooltip>
+
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Add friend</DialogTitle>
+					<DialogDescription>
+						Send a request to connect with your friends!
+					</DialogDescription>
+				</DialogHeader>
+
+				<Form {...form}>
+					<form
+						onSubmit={form.handleSubmit(handleSubmit)}
+						className="space-y-8">
+						<FormField
+							control={form.control}
+							name="email"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Email</FormLabel>
+									<FormControl>
+										<Input placeholder="Email..." {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<DialogFooter>
+							<Button type="submit" disabled={false}>
+								Send
+							</Button>
+						</DialogFooter>
+					</form>
+				</Form>
+			</DialogContent>
+		</Dialog>
+	);
+};
+
+export default AddFriendDialog;
