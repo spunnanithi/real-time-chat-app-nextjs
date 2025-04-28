fd680700c0e9bc9ffbe2808334a0aec09a6f6f66
Sirasit Punnanithi <spunnanithi@gmail.com>
2025-04-28 13:40:48 -0700

    Created item list and conversation container and fallback

--- app/(root)/friends/page.tsx
+++ app/(root)/friends/page.tsx
@@ -1,9 +1,16 @@
+import ConversationFallback from "@/components/shared/conversation/ConversationFallback";
+import ItemList from "@/components/shared/item-list/ItemList";
 import React from "react";
 
 type Props = {};
 
 const FriendsPage = (props: Props) => {
-	return <div>FriendsPage</div>;
+	return (
+		<>
+			<ItemList title="Friends">Friends Page</ItemList>
+			<ConversationFallback />
+		</>
+	);
 };
 
 export default FriendsPage;
--- components/shared/sidebar/nav/MobileNav.tsx
+++ components/shared/sidebar/nav/MobileNav.tsx
@@ -7,12 +7,17 @@ import {
 	TooltipContent,
 	TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { useConversation } from "@/hooks/useConversation";
 import { useNavigation } from "@/hooks/useNavigation";
 import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 const MobileNav = () => {
 	const paths = useNavigation();
+
+	const { isActive } = useConversation();
+
+	if (isActive) return null;
 
 	return (
 		<Card className="fixed bottom-4 w-[calc(100%-32px)] flex items-center h-16 p-2 lg:hidden">
