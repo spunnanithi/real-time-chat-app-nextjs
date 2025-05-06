5cfd19859a38022df9c491871850bf4e091f0241
Sirasit Punnanithi <spunnanithi@gmail.com>
2025-05-06 15:27:51 -0700

    Added Convex API to create a group chat

--- convex/conversation.ts
+++ convex/conversation.ts
@@ -1,5 +1,5 @@
 import { ConvexError, v } from "convex/values";
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { getUserByClerkId } from "./_utils";
 
 export const get = query({
@@ -61,6 +61,65 @@ export const get = query({
 				},
 				otherMembers: null,
 			};
+		} else {
+			const otherMembers = (
+				await Promise.all(
+					allConversationMemberships.filter(
+						(membership) => membership.memberId !== currentUser._id
+					)
+				)
+			).map(async (membership) => {
+				const member = await ctx.db.get(membership.memberId);
+
+				if (!member) {
+					throw new ConvexError("Member could not be found");
+				}
+
+				return {
+					username: member.username,
+				};
+			});
+
+			return { ...conversation, otherMembers, otherMember: null };
 		}
+	},
+});
+
+export const createGroup = mutation({
+	args: {
+		members: v.array(v.id("users")),
+		name: v.string(),
+	},
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity(); // Get the authenticated user's identity
+
+		// Ensure the user is authenticated
+		if (!identity) {
+			throw new Error("Unauthorized");
+		}
+
+		// Fetch the current user's record from the database using their Clerk ID
+		const currentUser = await getUserByClerkId({
+			ctx,
+			clerkId: identity.subject,
+		});
+
+		if (!currentUser) {
+			throw new ConvexError("User not found");
+		}
+
+		const conversationId = await ctx.db.insert("conversations", {
+			isGroup: true,
+			name: args.name,
+		});
+
+		await Promise.all(
+			[...args.members, currentUser._id].map(async (memberId) => {
+				ctx.db.insert("conversationMembers", {
+					memberId,
+					conversationId,
+				});
+			})
+		);
 	},
 });
