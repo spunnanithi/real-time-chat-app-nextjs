6aa4c741d1f5b6c0794290b1f7189e24d616c23e
Sirasit Punnanithi <spunnanithi@gmail.com>
2025-05-07 12:59:43 -0700

    Bug fix: fixed group chat tab on conversations not working correctly

--- convex/conversation.ts
+++ convex/conversation.ts
@@ -62,23 +62,21 @@ export const get = query({
 				otherMembers: null,
 			};
 		} else {
-			const otherMembers = (
-				await Promise.all(
-					allConversationMemberships.filter(
-						(membership) => membership.memberId !== currentUser._id
-					)
-				)
-			).map(async (membership) => {
-				const member = await ctx.db.get(membership.memberId);
+			const otherMembers = await Promise.all(
+				allConversationMemberships
+					.filter((membership) => membership.memberId !== currentUser._id)
+					.map(async (membership) => {
+						const member = await ctx.db.get(membership.memberId);
 
-				if (!member) {
-					throw new ConvexError("Member could not be found");
-				}
+						if (!member) {
+							throw new ConvexError("Member could not be found");
+						}
 
-				return {
-					username: member.username,
-				};
-			});
+						return {
+							username: member.username,
+						};
+					})
+			);
 
 			return { ...conversation, otherMembers, otherMember: null };
 		}
